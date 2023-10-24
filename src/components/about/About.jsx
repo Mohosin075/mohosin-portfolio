2d393ff2c60495dabde2a161d8605ee86f6a4ba0
Mohosin <mohosinali075@gmail.com>
2023-10-24 17:54:10 +0600

    image link change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
         <div className="w-10/12 mx-auto mt-5 md:mt-10">
             <div className="w-1/3 mb-10 mx-auto">
             <img className="rounded-full"
-          src="https://scontent.fdac24-3.fna.fbcdn.net/v/t39.30808-6/361644734_814889220343418_9103034117218873356_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFW6prJO9otsEXFqm7Ol-IweJBQtlJvY9t4kFC2Um9j2zmXzU7gGMJuB71y-lTJre2sZiTH-e-v5AcRIheVMFt7&_nc_ohc=dcHZQMcJbX0AX_xZp_z&_nc_ht=scontent.fdac24-3.fna&oh=00_AfDhgGYY1xca2BHrvfBLh5OSmJag3XVWs3sRsuOgryGS5Q&oe=6532015E"
+          src="https://i.ibb.co/FKHwG3N/mohosin.jpg"
           alt=""
         />
             </div>
